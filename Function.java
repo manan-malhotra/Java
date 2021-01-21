929d33a9e3280c2f796469666482bbdd0c151576
Manan Malhotra <mananmalhotra80@gmail.com>
2021-01-21 23:16:00 +0530

    Testing

---
+++ Function.java
@@ -0,0 +1,12 @@
+public class Function{
+    public static int test() {
+        System.out.println("Hello");
+        return 0;
+    }
+    public static void main(String[] args) {
+        test();
+        test();
+        test();
+    }
+    
+}
--- Hello.java
+++ Hello.java
@@ -1,4 +1,3 @@
-package com.manan;
 public class Hello{
     public static void main(String[] args) {
         System.out.println("Hello world");
