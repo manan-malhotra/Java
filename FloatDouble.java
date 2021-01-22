4c9da5c5d3c6732c7f804a64b56711aeb6641802
Manan Malhotra <mananmalhotra80@gmail.com>
2021-01-22 13:38:58 +0530

    Float and Double

---
+++ FloatDouble.java
@@ -0,0 +1,14 @@
+public class FloatDouble{
+    public static void main(String[] args) {
+        float a;
+        double b;
+        float c=(float)5.0;
+        float d=5.02323f;
+        a=Float.MAX_VALUE;
+        b=Double.MAX_VALUE;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+    }
+}
