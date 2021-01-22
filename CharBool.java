8ad62dc024eaa9c30c7a38029487faa1167e5309
Manan Malhotra <mananmalhotra80@gmail.com>
2021-01-22 13:59:17 +0530

    Bool and Char

---
+++ CharBool.java
@@ -0,0 +1,10 @@
+public class CharBool{
+    public static void main(String[] args) {
+        char myChar='A';
+        boolean isRight=true;
+        boolean isWrong=false;
+        System.out.println(isRight);
+        System.out.println(isWrong);
+        System.out.println(myChar);
+    }
+}
