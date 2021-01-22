1c514e607d87a2ca9560122ffa835ff084e18c33
Manan Malhotra <mananmalhotra80@gmail.com>
2021-01-22 13:33:08 +0530

    Datatypes

---
+++ Datatypes.java
@@ -0,0 +1,16 @@
+public class Datatypes{
+    public static void main(String[] args) {
+        byte a;
+        short b;
+        int c;
+        long d;
+        a=Byte.MAX_VALUE;
+        b=Short.MAX_VALUE;
+        c=Integer.MAX_VALUE;
+        d=Long.MAX_VALUE;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+    }
+}
